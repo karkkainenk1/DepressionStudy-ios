3b54ac10bf12e6075589e693450ab99ce347d3b3
Admin <admin@wifi-131-179-13-185.host.ucla.edu>
2018-07-17 14:55:37 -0700

    current

--- Health/ViewController.swift
+++ Health/ViewController.swift
@@ -10,18 +10,10 @@ import UIKit
 import AWAREFramework
 
 class ViewController: UIViewController {
-    // buttons for interface
-    @IBOutlet weak var welcome: UILabel!
-    @IBOutlet var subjectName: UILabel!
-    @IBOutlet var subjectID: UITextField!
-    @IBAction func submitID(_ sender: UIButton) {
-        subjectName.text = subjectID.text
-        subjectID.text = ""
-        welcome.text = "Welcome"
-        welcome.textAlignment = .center
-        subjectName.textAlignment = .center
+    // test buttons for interface
+    @IBAction func TestID(_ sender: UIButton) {
+        startSubjectID()
     }
-    
     @IBAction func TestESM(_ sender: UIButton) {
         startESM()
     }
@@ -100,8 +92,29 @@ class ViewController: UIViewController {
         }
     }
     func startSubjectID() {
+        // Create a FreeText ESM that essentially accepts subject ID to store in DB
+        let schedule = ESMSchedule.init()
+        let text = ESMItem.init(asNumericESMWithTrigger: "ID")
+        text?.setTitle("Study ID")
+        text?.setInstructions("Enter your unique ID here:")
+        text?.setExpirationWithMinute(10)
+        text?.setSubmitButtonName("Submit")
+        schedule.addESM(text)
+        let esmManager = ESMScheduleManager.shared()
         
+        // testing start (uncomment below when testing)
+        esmManager?.removeAllNotifications()
+        esmManager?.removeAllESMHitoryFromDB()
+        esmManager?.removeAllSchedulesFromDB()
+        // testing end (comment above when finished testing)
+        
+        esmManager?.add(schedule)
+        // allows viewcontrol to display ESM
+        viewDidAppear()
+        
+        //esmManager?.removeAllSchedulesFromDB()
     }
+ 
     //generic radio creation function
     func radioButtons(radioNum: Int, radioInstr: String, sch: ESMSchedule) {
         let str1 = String(radioNum) + "_radio"
@@ -155,65 +168,4 @@ class ViewController: UIViewController {
         viewDidAppear()
     }
 }
-//----------------------------------------------- Obsolete code... ---------------------------------------------------
-/* let radio1 = ESMItem.init(asRadioESMWithTrigger: "1_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio1?.setTitle("[1 of 10] During the past day, about how often did you feel ...")
- radio1?.setInstructions("... tired out for no good reason?")
- radio1?.setExpirationWithMinute(5)
- radio1?.setSubmitButtonName("Next")
- schdule.addESM(radio1)
- let radio2 = ESMItem.init(asRadioESMWithTrigger: "2_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio2?.setTitle("[2 of 10] During the past day, about how often did you feel ...")
- radio2?.setInstructions("... nervous?")
- radio2?.setExpirationWithMinute(5)
- radio2?.setSubmitButtonName("Next")
- schdule.addESM(radio2)
- let radio3 = ESMItem.init(asRadioESMWithTrigger: "3_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio3?.setTitle("[3 of 10] During the past day, about how often did you feel ...")
- radio3?.setInstructions("... so nervous that nothing could calm you down?")
- radio3?.setExpirationWithMinute(5)
- radio3?.setSubmitButtonName("Next")
- schdule.addESM(radio3)
- let radio4 = ESMItem.init(asRadioESMWithTrigger: "4_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio4?.setTitle("[4 of 10] During the past day, about how often did you feel ...")
- radio4?.setInstructions("... hopeless?")
- radio4?.setExpirationWithMinute(5)
- radio4?.setSubmitButtonName("Next")
- schdule.addESM(radio4)
- let radio5 = ESMItem.init(asRadioESMWithTrigger: "5_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio5?.setTitle("[5 of 10] During the past day, about how often did you feel ...")
- radio5?.setInstructions("... restless or fidgety?")
- radio5?.setExpirationWithMinute(5)
- radio5?.setSubmitButtonName("Next")
- schdule.addESM(radio5)
- let radio6 = ESMItem.init(asRadioESMWithTrigger: "6_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio6?.setTitle("[6 of 10] During the past day, about how often did you feel ...")
- radio6?.setInstructions("... so restless that you could not sit still?")
- radio6?.setExpirationWithMinute(5)
- radio6?.setSubmitButtonName("Next")
- schdule.addESM(radio6)
- let radio7 = ESMItem.init(asRadioESMWithTrigger: "7_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio7?.setTitle("[7 of 10] During the past day, about how often did you feel ...")
- radio7?.setInstructions("... depressed?")
- radio7?.setExpirationWithMinute(5)
- radio7?.setSubmitButtonName("Next")
- schdule.addESM(radio7)
- let radio8 = ESMItem.init(asRadioESMWithTrigger: "8_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio8?.setTitle("[8 of 10] During the past day, about how often did you feel ...")
- radio8?.setInstructions("... so depressed that nothing could cheer you up?")
- radio8?.setExpirationWithMinute(5)
- radio8?.setSubmitButtonName("Next")
- schdule.addESM(radio8)
- let radio9 = ESMItem.init(asRadioESMWithTrigger: "9_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio9?.setTitle("[9 of 10] During the past day, about how often did you feel ...")
- radio9?.setInstructions("... that everything was an effort?")
- radio9?.setExpirationWithMinute(5)
- radio9?.setSubmitButtonName("Next")
- schdule.addESM(radio9)
- let radio10 = ESMItem.init(asRadioESMWithTrigger: "10_radio", radioItems: ["None of the time", "A little of the time","Some of the time","Most of the time","All of the time"])
- radio10?.setTitle("[10 of 10] During the past day, about how often did you feel ...")
- radio10?.setInstructions("... worthless?")
- radio10?.setExpirationWithMinute(5)
- radio10?.setSubmitButtonName("Next")
- schdule.addESM(radio10)
- */
+

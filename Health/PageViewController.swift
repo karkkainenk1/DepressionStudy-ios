2d41cbcb5f05d8684446894218d01c078e751b7d
Admin <admin@wifi-131-179-13-124.host.ucla.edu>
2018-07-30 16:23:21 -0700

    current

--- Health/PageViewController.swift
+++ Health/PageViewController.swift
@@ -41,7 +41,7 @@ class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UI
     }
     
     func configurePageControl() {
-        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width / 2,height: 50))
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
         self.pageControl.numberOfPages = orderedViewControllers.count
         self.pageControl.currentPage = 0
         self.pageControl.tintColor = UIColor.black
